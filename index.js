bb3b3863ca2bed677d8dd1faaadbebd733cc064e
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-11-29 16:48:19 +0300

    start express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server on ${PORT}`);
+});
