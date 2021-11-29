87cca7b3028a96411933fe5a4df805755b8c60f0
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-11-29 20:33:41 +0300

    create Role contoller with addRole fun

---
+++ routers/controllers/role.js
@@ -0,0 +1,21 @@
+const roleModel = require("./../../db/models/role");
+
+const addRole = (req, res) => {
+  const { role, permissions } = req.body;
+
+  const newRole = new roleModel({
+    role,
+    permissions,
+  });
+
+  newRole
+    .save()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+
+module.exports = { addRole };
