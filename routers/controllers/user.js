907b13a80f140bc97d46bab236f368cc4f36fca5
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-11-29 21:27:42 +0300

    update user controllers with login fun

--- routers/controllers/user.js
+++ routers/controllers/user.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 const SALT = Number(process.env.SALT);
+const secretKey = process.env.secretKey;
 
 const register = async (req, res) => {
   const { email, password, role } = req.body;
@@ -25,3 +26,41 @@ const register = async (req, res) => {
       res.status(400).json(err);
     });
 };
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  userModel
+    .findOne({ email })
+    .then(async (result) => {
+      if (result) {
+        if (email === result.email) {
+          const hashedPass = await bcrypt.compare(password, result.password);
+
+          if (hashedPass) {
+            const payload = {
+              role: result.role,
+            };
+            const options = {
+              expiresIn: "60m",
+            };
+
+            const token = await jwt.sign(payload, secretKey, options);
+
+            res.status(200).json({ result, token });
+          } else {
+            res.status(400).json("email or password is not correct");
+          }
+        } else {
+          res.status(400).json("email or password is not correct");
+        }
+      } else {
+        res.status(400).json("email not found");
+      }
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+module.exports = { register, login };
