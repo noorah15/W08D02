aae44c8f128b6f2fa7f9f62f783a32c63bf9991d
noorah15 <noorah.sulaiman.elq.js@tuwaiq.edu.sa>
2021-11-29 21:35:22 +0300

    update index.js with user middleware

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(express.json());
 const role = require("./routers/routes/role");
 app.use(role);
 
+const user = require("./routers/routes/user");
+app.use(user);
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server on ${PORT}`);
